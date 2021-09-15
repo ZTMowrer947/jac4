4dbf6a18e81d107049220ede4ed0d87f2bcb4874
ZTMowrer947 <25135050+ZTMowrer947@users.noreply.github.com>
2021-09-15 19:19:44 -0400

    Add and style buttons for home screen
    Co-authored-by: mariam-mimi <mariam-mimi@users.noreply.github.com>

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.mainContent}>
+        <Button title="Play" />
+      </View>
+
+      <View style={styles.bottomBtns}>
+        <Button title="Music" />
+        <Button title="Coding Club!" />
+        <Button title="GitHub" />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +22,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  mainContent: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  bottomBtns: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    width: "25%",
+    marginBottom: "1rem",
   },
 });
