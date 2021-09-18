0a32ea1e05615f93475af59f080e45089f18615f
ZTMowrer947 <25135050+ZTMowrer947@users.noreply.github.com>
2021-09-18 12:37:16 -0400

    Add very sad GameSetup screen

--- screens/GameSetup.js
+++ screens/GameSetup.js
@@ -1,10 +1,28 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { Button, View, Text, StyleSheet } from "react-native";
 
-export default function GameSetupScreen() {
+export default function GameSetupScreen({ navigation }) {
   return (
     <View style={styles.container}>
-      <Text>GameSetup works</Text>
+      <View style={styles.mainContent}>
+        <Text>Game Setup</Text>
+        <View style={styles.btnGroup}>
+          <Button title="Singleplayer" />
+          <Button title="Multiplayer" />
+        </View>
+      </View>
+      <View style={styles.mainContent}>
+        <View style={styles.btnGroup}>
+          <Button title="Easy" />
+          <Button title="Medium" />
+          <Button title="Hard" />
+          <Button title="Extreme" />
+        </View>
+      </View>
+      <View style={styles.btnGroup}>
+        <Button title="Back" onPress={() => navigation.navigate("Home")} />
+        <Button title="Start!" />
+      </View>
     </View>
   );
 }
@@ -15,5 +33,20 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+    width: "100%",
+  },
+  mainContent: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    width: "100%",
+  },
+  btnGroup: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "flex-end",
+    width: "80%",
+    marginTop: "-10%",
+    paddingBottom: "10%",
   },
 });
