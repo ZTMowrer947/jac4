e9636459683a5dff0dfd0776efd1d92065e9bf5a
ZTMowrer947 <25135050+ZTMowrer947@users.noreply.github.com>
2021-09-19 09:34:14 -0400

    Configure Metro to process SVGs

---
+++ metro.config.js
@@ -0,0 +1,17 @@
+const { getDefaultConfig } = require("@expo/metro-config");
+
+const defaultConfig = getDefaultConfig(__dirname);
+
+defaultConfig.resolver.assetExts = defaultConfig.resolver.assetExts.filter(
+  (ext) => ext !== "svg"
+);
+defaultConfig.resolver.sourceExts = [
+  ...defaultConfig.resolver.sourceExts,
+  "svg",
+];
+
+defaultConfig.transformer.babelTransformerPath = require.resolve(
+  "react-native-svg-transformer"
+);
+
+module.exports = defaultConfig;
